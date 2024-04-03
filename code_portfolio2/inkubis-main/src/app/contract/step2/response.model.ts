f7167545db05fe3689f68a6196c7f909a1c6f8e1
Quincy van Deursen <q.vandeursen@student.avans.nl>
2024-04-03 19:35:03 +0200

    Front-end verwijderd

--- code_portfolio2/inkubis-main/src/app/contract/step2/response.model.ts
+++
@@ -1,9 +0,0 @@
-import { IField } from './field.model';
-
-export interface IResponse {
-  fieldId?: string;
-  name?: string;
-  data?: string;
-  field?: IField;
-}
-
--- code_portfolio2/inkubis-main/src/app/login/loginUser.model.ts
+++
@@ -1,9 +0,0 @@
-export class LoginUser {
-    email = '';
-    password = '';
-
-    constructor(email = '', password = '') {
-        this.email = email;
-        this.password = password;
-    }
-}
